fa4be69f4d61fc7aa4e66cc4d687dc228a0bb834
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-10-02 18:53:18 +0100

    add route to Properties and AddProperty components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import "../styles/App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="add-property" element={<AddProperty />} />
+      </Routes>
     </div>
   );
 };
