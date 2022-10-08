e944225469eb634e42f97b6ef44b2a6c1c7b2886
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-10-08 11:55:56 +0100

    sort by price added, amended property-card css

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/sidebar.css";
 
 const SideBar = () => {
+  const buildQueryString = (operation, valueObj) => {
+    const { search } = useLocation();
+
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="sidebar">
       <h2>Filter By City</h2>
-      <Link to={`/?query={"city": "Liverpool"}`}>Liverpool</Link>
-      <Link to={`/?query={"city": "Manchester"}`}>Manchester</Link>
-      <Link to={`/?query={"city": "Leeds"}`}>Leeds</Link>
-      <Link to={`/?query={"city": "Sheffield"}`}>Sheffield</Link>
+      <Link to={buildQueryString("query", { city: "Liverpool" })}>
+        Liverpool
+      </Link>
+      <Link to={buildQueryString("query", { city: "Manchester" })}>
+        Manchester
+      </Link>
+      <Link to={buildQueryString("query", { city: "Leeds" })}>Leeds</Link>
+      <Link to={buildQueryString("query", { city: "Sheffield" })}>
+        Sheffield
+      </Link>
+      <h2>Sort by</h2>
+      <Link to={buildQueryString("sort", { price: 1 })}>Price Ascending</Link>
+      <Link to={buildQueryString("sort", { price: -1 })}>Price Descending</Link>
     </div>
   );
 };
